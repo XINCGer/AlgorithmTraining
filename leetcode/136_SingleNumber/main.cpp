b2de9e66194c4fa68f9f6517cbe6df6c67e56cc2
马三小伙儿 <1790617178@qq.com>
2018-03-15 23:48:49 +0800

    利用数组排序AC

--- leetcode/136_SingleNumber/main.cpp
+++ leetcode/136_SingleNumber/main.cpp
@@ -6,25 +6,34 @@
 using namespace std;
 
 
-int singleNumber(vector<int>& nums)
-{
-    if(nums.empty())
-        return 0;
-    map<int,int> m;
-    for(int i=0; i<nums.size(); i++)
-    {
-        m[nums[i]]++;
-    }
-    for(map<int,int>::iterator it = m.begin(); it!=m.end(); it++)
-    {
-        if((*it).second == 1)
-        {
-            return (*it).first;
+//int singleNumber(vector<int>& nums)
+//{
+//    if(nums.empty())
+//        return 0;
+//    map<int,int> m;
+//    for(int i=0; i<nums.size(); i++)
+//    {
+//        m[nums[i]]++;
+//    }
+//    for(map<int,int>::iterator it = m.begin(); it!=m.end(); it++)
+//    {
+//        if((*it).second == 1)
+//        {
+//            return (*it).first;
+//        }
+//    }
+//    return 0;
+//}
+
+int singleNumber(vector<int>& nums){
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<nums.size()-2;i+=2){
+        if(nums[i]!=nums[i+1]){
+            return nums[i];
         }
     }
-    return 0;
+    return nums[nums.size()-1];
 }
-
 
 int main()
 {
