de7dfb53ca0f913312a1c44ecf567e5fa044cb3c
马三小伙儿 <1790617178@qq.com>
2018-08-31 19:39:38 +0800

    递归实现字符串全排列

--- 编程之法面试和算法心得/字符串全排列/main.cpp
+++ 编程之法面试和算法心得/字符串全排列/main.cpp
@@ -4,8 +4,28 @@
 
 using namespace std;
 
+//µ›πÈ µœ÷
+void CalcAllPermutation(string &s,int from , int to){
+    if(to<=1){
+        return;
+    }
+    if(from == to){
+        for(int i=0;i<=to;i++){
+            cout<<s[i];
+        }
+        cout<<endl;
+    }
+    else{
+        for(int j=from;j<=to;j++){
+            swap(s[j],s[from]);
+            CalcAllPermutation(s,from+1,to);
+            swap(s[j],s[from]);
+        }
+    }
+}
 int main()
 {
-
+    string ss= "abcd";
+    CalcAllPermutation(ss,0,ss.size()-1);
     return 0;
 }
