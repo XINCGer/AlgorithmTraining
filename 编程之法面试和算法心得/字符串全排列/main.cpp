d9e03e2df3bbd6a85901cba52a15f5a387ec1f49
马三小伙儿 <1790617178@qq.com>
2018-08-31 19:53:08 +0800

    全排列字典序实现

--- 编程之法面试和算法心得/字符串全排列/main.cpp
+++ 编程之法面试和算法心得/字符串全排列/main.cpp
@@ -23,6 +23,30 @@ void CalcAllPermutation(string &s,int from , int to){
         }
     }
 }
+
+//字典序
+bool CalcAllPermutationDic(string &s, int num){
+    int i;
+    //step1:找出排列中最后(最右)一个升序的首位位置i, x=ai
+    for(int i=num-2;(i>=0) && (s[i]>=s[i+1]); --i){
+        ;
+    }
+    //已经找到所有排列
+    if(i<0){
+        return false;
+    }
+    int k;
+    //step2:找到排列中第i位右边最后一个比ai大的位置j,y=ai
+    for(k = num-1;(k>i) && (s[k]<=s[i]);--k){
+        ;
+    }
+    //step3:交换x,y
+    swap(s[i],s[k]);
+    //step4:把第i+1位到最后的部分翻转;
+    reverse(s.begin()+i+1,s.begin()+num);
+    return true;
+}
+
 int main()
 {
     string ss= "abcd";
